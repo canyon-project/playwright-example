6fa3e34ec2fb5dee69fa55a98767ff64a54bb883
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-06-22 15:36:35 +0800

    init

--- canyon-uploader.js
+++ canyon-uploader.js
@@ -43,5 +43,15 @@ jsonFiles.forEach((file,index) => {
 // console.log(map)
 
 for (const [key,value] of map) {
-    fs.writeFileSync(key+'.json',JSON.stringify(value))
+    // fs.writeFileSync(key+'.json',JSON.stringify(value))
+    await fetch('https://app.canyoncov.com/coverage/client',{
+        method:'POST',
+        body:JSON.stringify(value),
+        headers: {
+            Authorization:`Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IndyX3poYW5nMjUiLCJpZCI6MTAxMjA1ODAsImlhdCI6MTcxODQzMjc1OSwiZXhwIjoyMDM0MDA4NzU5fQ.2C7hUfC_uVykgvr4y4cw3SW9BO5K187921IVdrFGY9c`,
+            'Content-Type': 'application/json' // 指定请求头中的内容类型为 JSON
+        },
+    }).then(res=>res.json()).then(data=>{
+        console.log(data)
+    })
 }
